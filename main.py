6f8d51c91089bed5334e5134d24c501401a7d14d
Kirill <mikirus46@gmail.com>
2021-01-20 23:03:56 +0300

    бот точно работает. только костыли на поднятия не работают

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import time
 import sys
 import os
 
-
 TELEGRAM_API_TOKEN = '1152345546:AAH0m8CDNkNwqNlnk19EY7qZrPbZlA3kK5c'
 bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
@@ -18,103 +17,137 @@ bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):  # Название функции не играет никакой роли
     print('text')
-    bot.send_message(message.chat.id, message.text)
+    send_msg(message, message.text)
+
+
+# @bot.message_handler(content_types=['document'])
+# def send_text(message):
+#     file_id_info = bot.get_file(message.document.file_id)
+#     downloaded_file = bot.download_file(file_id_info.file_path)
+#     with open("new_file.wav", 'wb') as new_file:
+#         new_file.write(downloaded_file)
+#     go_tf()
+#     bot.send_message(message.chat.id, "+")
 
 
 @bot.message_handler(content_types=['voice'])
 def voice_processing(message):
-    file_info = bot.get_file(message.voice.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    with open('new_file.ogg', 'wb') as new_file:
-        new_file.write(downloaded_file)
+    send_msg(message, "No")
 
-    audiosegment.converter = r"C:/ffmpeg/bin/ffmpeg.exe"
-    audiosegment.ffprobe = r"C:/ffmpeg/bin/ffprobe.exe"
-    sound = audiosegment.from_file("new_file.ogg")
-    sound.export("new_file.wav", format="wav")
-    myModel = myTF.keras.models.load_model("Examples/1/myModel.h5")
 
-    wav_name = 'new_file.wav'
-    # wav_name = 'Control.wav'
-    # wav_name = 'Bad.wav'
+@bot.message_handler(content_types=['document'])
+def handle_docs_photo(message):
+    try:
 
-    types = {
-        1: np.int8,
-        2: np.int16,
-        4: np.int32
-    }
+        file_info = bot.get_file(message.document.file_id)
+        send_msg(message, "Не переживай, я начал скачивать твой файл")
+        downloaded_file = bot.download_file(file_info.file_path)
+        print('name FILE', file_info.file_path)
+        name_wav = "new_file.wav"
+        with open(name_wav, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.reply_to(message, "Скачал, сейчас посмотрим....")
 
-    def format_time(x, pos=None):
-        global duration, nframes, k
-        progress = int(x / float(nframes) * duration * k)
-        mins, secs = divmod(progress, 60)
-        hours, mins = divmod(mins, 60)
-        out = "%d:%02d" % (mins, secs)
-        if hours > 0:
-            out = "%d:" % hours
-        return out
+        myModel = myTF.keras.models.load_model("Examples/1/myModel.h5")
 
-    def format_db(x, pos=None):
-        if pos == 0:
-            return ""
-        global peak
-        if x == 0:
-            return "-inf"
+        wav_name = 'new_file.wav'
 
-        db = 20 * math.log10(abs(x) / float(peak))
-        return int(db)
+        types = {
+            1: np.int8,
+            2: np.int16,
+            4: np.int32
+        }
 
-    wav = wave.open(wav_name, mode="r")
-    (nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
+        def format_time(x, pos=None):
+            global duration, nframes, k
+            progress = int(x / float(nframes) * duration * k)
+            mins, secs = divmod(progress, 60)
+            hours, mins = divmod(mins, 60)
+            out = "%d:%02d" % (mins, secs)
+            if hours > 0:
+                out = "%d:" % hours
+            return out
 
-    content = wav.readframes(nframes)
-    samples = np.frombuffer(content, dtype=types[sampwidth])
-    channel = samples[1::nchannels]
-    print(len(channel))
-    # averageAmplitude = (sum(abs(channel)) / len(channel))*4
-    # print(averageAmplitude)
-    #
-    # i = 0
-    # listSamples = []
-    # while i < len(channel):
-    #     # print(abs(channel[i]))
-    #     if abs(channel[i]) > averageAmplitude:
-    #         NewPosition = round(i + (framerate * 0.4))
-    #         listSamples.append(np.array(channel[i:NewPosition], dtype=types[sampwidth]))
-    #         i = NewPosition
-    #     i += 1
+        def format_db(x, pos=None):
+            if pos == 0:
+                return ""
+            global peak
+            if x == 0:
+                return "-inf"
 
-    startPosition = np.argmax(channel) - 3000
-    finishPosition = round(startPosition + (framerate * 0.4))
-    sample = np.array(channel[startPosition:finishPosition], dtype=types[sampwidth])
-    print(sample)
-    frequencies, times, spectrogram = sg.spectrogram(sample, framerate, nfft=48000)
-    i1size = spectrogram.shape[0]
-    i2size = spectrogram.shape[1]
-    spectrogram2 = np.zeros((i1size))
+            db = 20 * math.log10(abs(x) / float(peak))
+            return int(db)
 
-    i1 = 0
-    for i1 in range(i1size):
-        for i2 in range(i2size):
-            spectrogram2[i1] = spectrogram2[i1] + spectrogram[i1, i2]
-    t = np.arange(0, i1size, 1)
-    fig, ax = plt.subplots()
-    ax.plot(t, spectrogram2)
-    ax.grid()
-    fig.savefig(wav_name + '_1d.png')
-    # plt.show()
-    print(len(spectrogram2))
-    Value1 = list(spectrogram2).index(max(spectrogram2[0:200])) - 107.605
-    Value2 = list(spectrogram2).index(max(spectrogram2[300:500])) - 447.965
-    Value3 = list(spectrogram2).index(max(spectrogram2[600:1000])) - 713.425
-    Value4 = list(spectrogram2).index(max(spectrogram2[1100:1500])) - 1354.31
-    Value5 = list(spectrogram2).index(max(spectrogram2[1600:2000])) - 1785.205
+        wav = wave.open(wav_name, mode="r")
+        (nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
+        print(nchannels, sampwidth, framerate, nframes, comptype, compname)
+        # duration = nframes / framerate
+        # w, h = 800, 300
+        # k = nframes/w/32
+        # # k = nframes
+        # DPI = 72
+        # peak = 256 ** sampwidth / 2
 
-    print(myModel.predict([[Value1, Value2, Value3, Value4, Value5]]))
-    jopa = myModel.predict([[Value1, Value2, Value3, Value4, Value5]])
-    bot.send_message(message.chat.id, str(jopa) + "ответ")
-    bot.send_photo(message.chat.id, open(wav_name + '_1d.png', 'rb'))
-    print('finish one')
+        content = wav.readframes(nframes)
+        print('jopa tut bila', types[sampwidth])
+        samples = np.frombuffer(content, dtype=types[sampwidth])
+        channel = samples[1::nchannels]
+
+        averageAmplitude = (sum(abs(channel)) / len(channel)) * 2
+        print(averageAmplitude)
+
+        i = 0
+        listSamples = []
+        while i < len(channel):
+            # print(abs(channel[i]))
+            if abs(channel[i]) > averageAmplitude:
+                NewPosition = round(i + (framerate * 0.4))
+                listSamples.append(np.array(channel[i:NewPosition], dtype=types[sampwidth]))
+                i = NewPosition
+            i += 1
+
+        print(len(listSamples))
+        NaturalFrequencies = []
+        for sample in listSamples:
+            frequencies, times, spectrogram = sg.spectrogram(sample, framerate, nfft=4096)
+            # 2d spectrogramm -> 1d spectrogramm2
+            i1size = spectrogram.shape[0]
+            i2size = spectrogram.shape[1]
+            spectrogram2 = np.zeros((i1size))
+            i1 = 0
+            for i1 in range(i1size):
+                for i2 in range(i2size):
+                    spectrogram2[i1] = spectrogram2[i1] + spectrogram[i1, i2]
+            # #show 1d
+            t = np.arange(0, i1size, 1)
+            fig, ax = plt.subplots()
+            ax.plot(t, spectrogram2)
+            ax.grid()
+            fig.savefig('new_1d.png')
+            # plt.show()
+            NaturalFrequencies.append([])
+            # print(list(spectrogram2).index(max(spectrogram2[0:200])), list(spectrogram2).index(max(spectrogram2[300:500])), list(spectrogram2).index(max(spectrogram2[600:1000])), list(spectrogram2).index(max(spectrogram2[1100:1500])), list(spectrogram2).index(max(spectrogram2[1600:2000])))
+            Value1 = list(spectrogram2).index(max(spectrogram2[0:200])) - 107.605
+            Value2 = list(spectrogram2).index(max(spectrogram2[300:500])) - 447.965
+            Value3 = list(spectrogram2).index(max(spectrogram2[600:1000])) - 713.425
+            Value4 = list(spectrogram2).index(max(spectrogram2[1100:1500])) - 1354.31
+            Value5 = list(spectrogram2).index(max(spectrogram2[1600:2000])) - 1785.205
+            print(myModel.predict([[Value1, Value2, Value3, Value4, Value5]]))
+            bot.send_message(message.chat.id, myModel.predict([[Value1, Value2, Value3, Value4, Value5]]))
+            bot.send_photo(message.chat.id, open('new_1d.png', 'rb'))
+
+    except Exception as e:
+        bot.reply_to(message, e)
+
+
+# костыли на дисконект
+def send_msg(massage, msg):
+    try:
+        bot.send_message(massage.chat.id, msg)
+    except (ConnectionAbortedError, ConnectionResetError, ConnectionRefusedError, ConnectionError):
+        print("ConnectionError - Sending again after 5 seconds!!!")
+        time.sleep(5)
+        bot.send_message(massage.chat.id, msg)
 
 
 while True:
@@ -131,5 +164,3 @@ while True:
             saving data) must be done before calling this function."""
         python = sys.executable
         os.execl(python, python, *sys.argv)
-# if __name__ == '__main__':
-#     bot.infinity_polling()
